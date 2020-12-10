b2f24042dde1f805a62d29dc7bf459a5a2e9840a
Никита Пермяков <permikov134@mail.ru>
2020-12-10 14:07:53 +0500

    add sceleton

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 
 int main(){
-    //srand(static_cast<unsigned int>(time(0)));
+    srand(static_cast<unsigned int>(time(0)));
     sf::RenderWindow window(sf::VideoMode(400, 300), "NodeNetwork", sf::Style::None);
 
     return 0;
