a5856a5e3819bdda8b78957dae5690c822de699c
Никита Пермяков <permikov134@mail.ru>
2020-12-10 13:57:09 +0500

    add to gitignore

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+#include <SFML/Graphics.hpp>
+
+
+
+int main(){
+    //srand(static_cast<unsigned int>(time(0)));
+    sf::RenderWindow window(sf::VideoMode(400, 300), "NodeNetwork", sf::Style::None);
+
+    return 0;
+}
