045ad807a8ff10de052887d2b67b13f1995e9f32
Никита Пермяков <permikov134@mail.ru>
2020-12-11 02:43:19 +0500

    add cell

--- Cell.cpp
+++ Cell.cpp
@@ -3,13 +3,36 @@
 
 
 Cell::Cell(float radius, sf::Vector2f pos, float capacity) :
-    radius(radius), pos(pos), capacity(capacity) {
+    radius(radius), pos(pos), capacity(capacity),
+    idOwner(0), units(0), time(1.f) {
 
 }
 
 Cell::Cell(float radius, float px, float py, float capacity) :
-    radius(radius), pos(px, py), capacity(capacity) {
+    radius(radius), pos(px, py), capacity(capacity),
+    idOwner(0), units(0), time(1.f) {
 
 }
 
 Cell::~Cell() { }
+
+
+const float Cell::TIME_SCALE = 0.1f;
+
+void Cell::update(float dt) {
+    this->time -= dt * this->radius * this->TIME_SCALE;
+    if (this->time < 0.f) {
+        ++this->units;
+        if (this->units > this->capacity)
+            this->units = this->capacity;
+        this->time = 1.f;
+    }
+}
+
+void Cell::setOwner(int ownerId) {
+    this->idOwner = ownerId;
+}
+
+int Cell::getOwner() const {
+    return this->idOwner;
+}
--- Cell.h
+++ Cell.h
@@ -3,16 +3,31 @@
 #include <SFML/Network.hpp>
 #include <SFML/Graphics.hpp>
 
+class Gameboard;
 
 
 class Cell {
 public:
 	Cell(float radius, sf::Vector2f pos, float capacity);
 	Cell(float radius, float posx, float posy, float capacity);
-	virtual ~Cell();
+    virtual ~Cell();
+
+    void setOwner(int ownerId);
+    int getOwner() const;
+    void update(float dt);
 
 protected:
-	float radius;
-	sf::Vector2f pos;
-	sf::Uint16 capacity;
+    float radius;
+    sf::Vector2f pos;
+    sf::Uint16 capacity;
+
+    sf::Uint8 idOwner;
+    sf::Uint16 units;
+
+    float time;
+    static const float TIME_SCALE;
+
+private:
+	friend class Gameboard;
+	friend class Link;
 };
