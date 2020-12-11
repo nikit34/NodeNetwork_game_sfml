75437743ddef0592ed6f6fc6aab2bba3c86e1476
Никита Пермяков <permikov134@mail.ru>
2020-12-12 01:10:20 +0500

    add setup UDP connected

--- Server.cpp
+++ Server.cpp
@@ -17,6 +17,52 @@ void Server::launch() {
 	this->exitCurrentGame = false;
 
 	this->gboard.init();
+
+	sf::Thread thread(&Server::waitClient, &(*this));
+	thread.launch();
+
+	this->socket_udp.setBlocking(false);
+	this->menuWaitingRoom();
+
+	thread.terminate();
+
+	if (!this->window.isOpen())
+		return;
+	
+	if (this->connected) {
+		this->serverStatus = SERVER_START_GAME;
+		
+		sf::Packet packet;
+
+		for (std::vector<sf::TcpSocket*>::iterator it = this->sockets.begin(); it != this->sockets.end(); ++it) {
+			sf::TcpSocket& client = **it;
+			
+			packet << this->playerConnected << this->playerMax << this->serverStatus;
+
+			if (client.send(packet) == sf::Socket::Done)
+				std::cout << "Send push sequence to " << client.getRemoteAddress().toString() << std::endl;
+			
+		}
+
+		packet << this->gboard;
+
+		uint64_t id = 0;
+		for (std::vector<sf::TcpSocket*>::iterator it = this->sockets.begin(); it != this->sockets.end(); ++it) {
+			sf::TcpSocket& client = **it;
+			if (client.send(packet) == sf::Socket::Done)
+				std::cout << "Carte be send " << int(id + 2) << std::endl;
+			client.setBlocking(false);
+			++id;
+		}
+
+		this->timeElapsed.restart();
+		this->run();
+	}
+	else {
+		std::cout << "Solo game" << std::endl;
+		this->timeElapsed.restart();
+		this->run();
+	}
 }
 
 void Server::manageEvents(sf::Event event)
