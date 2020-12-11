c5f6d8f78569a5836745f6fde4fcead5c7afd7b2
Никита Пермяков <permikov134@mail.ru>
2020-12-12 00:15:04 +0500

    add server methods

--- Server.h
+++ Server.h
@@ -12,8 +12,24 @@ public:
 	Server(sf::RenderWindow& win);
 	virtual ~Server();
 
-	virtual void run();
 	void launch();
 
+	void manageEvents(sf::Event event);
+    virtual void actionLink(int orgX, int orgY, int destX, int destY);
+    virtual void actionFree(int mouseX, int mouseY);
+    virtual void actionFree(sf::Uint8 nidPlayer, int mouseX, int mouseY);
+    virtual void actionChangeLink(int orgX, int orgY, int destX, int destY);
+    void menuWaitingRoom();
+    void waitClient();
+    void waitAll();
+    virtual void manageNetwork();
+    void addPlayer();
 
+protected:
+    void resyncClients();
+    sf::TcpListener listener;
+    sf::SocketSelector selector;
+    std::vector<sf::TcpSocket*> sockets;
+    sf::Clock timeElapsed; 
+    static const sf::Int32 T_RESYNC = 1000; 
 };
