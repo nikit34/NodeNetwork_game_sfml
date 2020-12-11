55d2c528221bcba1e58b37b315419086297ca71c
Никита Пермяков <permikov134@mail.ru>
2020-12-12 00:20:49 +0500

    add sceleton clientmenuserver section

--- ClientMenuServerList.cpp
+++ ClientMenuServerList.cpp
@@ -1,1 +1,16 @@
 #include "ClientMenuServerList.h"
+
+
+
+MenuServerList::MenuServerList(sf::RenderWindow& win, sf::UdpSocket& sock, bool& co, sf::IpAddress& serv_add, sf::Uint8& servStatus)
+{
+}
+
+MenuServerList::~MenuServerList()
+{
+}
+
+
+void MenuServerList::run()
+{
+}
